6689eeb1fe6adaa38647d549791374d501adadce
James Taranto <James.Taranto@intrepidtravel.com>
2020-03-05 22:21:48 +1100

    Initial commit

---
+++ octoprint_microsoft_teams/static/js/microsoft_teams.js
@@ -0,0 +1,29 @@
+/*
+ * View model for OctoPrint-Microsoft_Teams
+ *
+ * Author: James Taranto
+ * License: AGPLv3
+ */
+$(function() {
+    function Microsoft_teamsViewModel(parameters) {
+        var self = this;
+
+        // assign the injected parameters, e.g.:
+        // self.loginStateViewModel = parameters[0];
+        // self.settingsViewModel = parameters[1];
+
+        // TODO: Implement your plugin's view model here.
+    }
+
+    /* view model class, parameters for constructor, container to bind to
+     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
+     * and a full list of the available options.
+     */
+    OCTOPRINT_VIEWMODELS.push({
+        construct: Microsoft_teamsViewModel,
+        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
+        dependencies: [ /* "loginStateViewModel", "settingsViewModel" */ ],
+        // Elements to bind to, e.g. #settings_plugin_microsoft_teams, #tab_plugin_microsoft_teams, ...
+        elements: [ /* ... */ ]
+    });
+});
